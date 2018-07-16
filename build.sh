cf22662e2814ab00acc3a774731bfd8c66f26beb
ramtob <ramtob@gmail.com>
2018-07-16 15:03:06 +0300

    feat(build.sh): providing a default value for projects dir

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
-# pass your projects dir path as a parameter (e.g. /c/projects)
+# you can pass your projects dir path as a parameter (e.g. /c/projects)
+# the default is the parent dir of the current dir
 
 PROJECTS_DIR=$1
+if [ -z "$PROJECTS_DIR" ]
+then
+	PROJECTS_DIR="${PWD}/.."
+fi
+
 ANSYN_ROOT="$PROJECTS_DIR/Ansyn/build/buildBundle"
 cd $ANSYN_ROOT
 
