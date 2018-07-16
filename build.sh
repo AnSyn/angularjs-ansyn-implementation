0d5867a54ced33abaceb7127c798b6d7dac7b551
YairTawil <yairtawil@gmail.com>
2018-07-16 12:13:14 +0300

    fix: rmv assets dir on build

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+set -e
+
 ANSYN_ROOT=~/Desktop/Development/Columbus/ansyn/build/buildBundle
 cd $ANSYN_ROOT
 
@@ -9,6 +11,7 @@ gulp createCdn
 
 cd ~/Desktop/Development/Columbus/ansyn-angular1
 cp $ANSYN_ROOT/deployNoZone/ansynNoZone.js ./app/scripts/
-\cp $ANSYN_ROOT/dist/fontawesome* ./app
-\cp $ANSYN_ROOT/dist/roboto* ./app
-\cp -r $ANSYN_ROOT/dist/assets ./app/assets
+cp $ANSYN_ROOT/dist/fontawesome* ./app
+cp $ANSYN_ROOT/dist/roboto* ./app
+rm -rf ./app/assets
+cp -r $ANSYN_ROOT/dist/assets ./app/assets
